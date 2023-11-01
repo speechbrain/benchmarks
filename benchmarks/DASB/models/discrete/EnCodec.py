288d28b2b9314b68dcf6033f95fd322b4792134a
poonehmousavi <moosavi.pooneh@gmail.com>
2023-11-01 17:15:00 -0400

    add comments

---
+++ benchmarks/DASB/models/discrete/EnCodec.py
@@ -0,0 +1,7 @@
+
+"""
+This lobe enables the integration of pretrained EnCodec.
+Reference: High Fidelity Neural Audio Compression (https://arxiv.org/pdf/2210.13438.pdf).
+Authors
+ * 
+"""
---
+++ benchmarks/DASB/models/discrete/Q-Hubert.py
@@ -0,0 +1,6 @@
+"""
+This lobe enables the integration of pretrained Q-Hubert.
+Reference: HubBERT (https://arxiv.org/pdf/2106.07447.pdf).
+Authors
+ * 
+"""

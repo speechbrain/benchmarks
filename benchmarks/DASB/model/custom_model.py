8772ae260af645c4483947e1514bf6bb1e0f3f22
poonehmousavi <moosavi.pooneh@gmail.com>
2024-04-25 10:44:10 -0400

    add docstring

--- benchmarks/DASB/model/custom_model.py
+++ benchmarks/DASB/model/custom_model.py
@@ -17,6 +17,41 @@ class AttentionMLP(torch.nn.Module):
 
 
 class Discrete_EmbeddingLayer(torch.nn.Module):
+    """This class handles embedding layers  for discrete tokens.
+
+    Arguments
+    ---------
+    num_codebooks: int ,
+        number of codebooks of the tokenizer.
+    vocab_size : int,
+        size of the dictionary of embeddings
+    emb_dim: int ,
+        the size of each embedding vector
+    pad_index: int (default: 0),
+        If specified, the entries at padding_idx do not contribute to the gradient.
+    init: boolean (default: False):
+        If set to True, init the embedding with the tokenizer embedding otherwise init randomly.
+    freeze: boolean (default: False)
+       If True, the embedding is frozen. If False, the model will be trained
+        alongside with the rest of the pipeline.
+
+    Example
+    -------
+    >>> from speechbrain.lobes.models.huggingface_transformers.encodec import Encodec
+    >>> model_hub = "facebook/encodec_24khz"
+    >>> save_path = "savedir"
+    >>> model = Encodec(model_hub, save_path)
+    >>> audio = torch.randn(4, 1000)
+    >>> length = torch.tensor([1.0, .5, .75, 1.0])
+    >>> tokens, emb = model.encode(audio, length)
+    >>> print(tokens.shape)
+    torch.Size([4, 4, 2])
+    >>> emb= Discrete_EmbeddingLayer(2, 1024, 1024)
+    >>> in_emb = emb(tokens)
+    >>> print(in_emb.shape)
+    torch.Size([4, 4, 2, 1024])
+    """
+
     def __init__(
         self,
         num_codebooks,
@@ -36,6 +71,18 @@ class Discrete_EmbeddingLayer(torch.nn.Module):
         #  TODO: handle padding tokens and initialization with embedding from codec
 
     def forward(self, in_tokens):
+        """Computes the embedding for discrete tokens.
+        a sample.
+
+        Arguments
+        ---------
+        in_tokens : torch.Tensor
+            A (Batch x Time x num_codebooks)
+            audio sample
+        Returns
+        -------
+        in_embs : torch.Tensor
+        """
         with torch.set_grad_enabled(not self.freeze):
             #  Add unique token IDs across diffrent codebooks by adding num_codebooks * vocab_size
             in_tokens += torch.arange(
@@ -47,56 +94,3 @@ class Discrete_EmbeddingLayer(torch.nn.Module):
             # Forward Pass to embedding and
             in_embs = self.embedding(in_tokens)
             return in_embs
-
-
-# from speechbrain.lobes.models.huggingface_transformers.encodec import Encodec
-# model_hub = "facebook/encodec_24khz"
-# save_path = "savedir"
-# model = Encodec(model_hub, save_path)
-# audio = torch.randn(4, 1000)
-# length = torch.tensor([1.0, .5, .75, 1.0])
-# tokens, emb = model.encode(audio, length)
-# print("hi")
-
-# import torch
-# from speechbrain.lobes.models.huggingface_transformers.discrete_ssl import DiscreteSSL
-# from speechbrain.lobes.models.huggingface_transformers.hubert import (HuBERT)
-# inputs = torch.rand([3, 2000])
-# model_hub = "facebook/hubert-large-ll60k"
-# save_path = "savedir"
-# ssl_layer_num = [7,23]
-# deduplicate =[False, True]
-# bpe_tokenizers=[None, None]
-# kmeans_repo_id = "poonehmousavi/SSL_Quantization"
-# kmeans_dataset = "LJSpeech"
-# num_clusters = 1000
-# ssl_model = HuBERT(model_hub, save_path,output_all_hiddens=True)
-# model = DiscreteSSL(save_path, ssl_model, kmeans_repo_id=kmeans_repo_id, kmeans_dataset=kmeans_dataset,num_clusters=num_clusters)
-# tokens, embs ,pr_tokens= model(inputs,SSL_layers=ssl_layer_num, deduplicates=deduplicate, bpe_tokenizers=bpe_tokenizers)
-
-# from speechbrain.lobes.models.discrete.dac import DAC
-
-# dac = DAC(load_pretrained=True, model_type="24khz", model_bitrate="8kbps", tag="latest")
-# # continuous_embeddings = torch.randn(1, 1024, 100) # Example shape: [Batch, Channels, Time]
-# audio = torch.randn(160, 72000)
-# # length = torch.tensor([1.0, .5, .75, 1.0])
-# tokens , _= dac(audio.unsqueeze(1),n_quantizers=2)
-# emb= Discrete_EmbeddingLayer(2, 1024, 1024)
-# in_emb = emb(tokens)
-# print(in_emb.shape)
-
-
-from speechbrain.lobes.models.discrete.speechtokenizer_interface import (
-    SpeechTokenizer_interface,
-)
-
-model_hub = "fnlp/SpeechTokenizer"
-save_path = "savedir"
-model = SpeechTokenizer_interface(model_hub, save_path)  # doctest: +SKIP
-# continuous_embeddings = torch.randn(1, 1024, 100) # Example shape: [Batch, Channels, Time]
-audio = torch.randn(160, 2000)
-# length = torch.tensor([1.0, .5, .75, 1.0])
-tokens = model(audio).permute(1, 2, 0)[:, :, :2]
-emb = Discrete_EmbeddingLayer(2, 1024, 1024)
-in_emb = emb(tokens)
-print(in_emb.shape)

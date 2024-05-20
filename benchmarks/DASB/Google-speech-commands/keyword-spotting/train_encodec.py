c8b6437413015fbc24c07dcc939c31216ca24c91
poonehmousavi <moosavi.pooneh@gmail.com>
2024-05-20 12:47:52 -0400

    update kS recepies

--- benchmarks/DASB/Google-speech-commands/keyword-spotting/train_encodec.py
+++ benchmarks/DASB/Google-speech-commands/keyword-spotting/train_encodec.py
@@ -119,17 +119,17 @@ class SpeakerBrain(sb.core.Brain):
                 self.model_optimizer, new_lr
             )
 
-            (
-                old_lr_encoder,
-                new_lr_encoder,
-            ) = self.hparams.lr_annealing_weights(stats["error_rate"])
-            sb.nnet.schedulers.update_learning_rate(
-                self.weights_optimizer, new_lr_encoder
-            )
+            # (
+            #     old_lr_encoder,
+            #     new_lr_encoder,
+            # ) = self.hparams.lr_annealing_weights(stats["error_rate"])
+            # sb.nnet.schedulers.update_learning_rate(
+            #     self.weights_optimizer, new_lr_encoder
+            # )
 
             # The train_logger writes a summary to stdout and to the logfile.
             self.hparams.train_logger.log_stats(
-                {"Epoch": epoch, "lr": old_lr, "att_lr": old_lr_encoder},
+                {"Epoch": epoch, "lr": old_lr},
                 train_stats={"loss": self.train_loss},
                 valid_stats=stats,
             )
@@ -148,22 +148,22 @@ class SpeakerBrain(sb.core.Brain):
 
     def init_optimizers(self):
         "Initializes the weights optimizer and model optimizer"
-        self.weights_optimizer = self.hparams.weights_opt_class(
-            self.hparams.attention_mlp.parameters()
-        )
+        # self.weights_optimizer = self.hparams.weights_opt_class(
+        #     self.hparams.attention_mlp.parameters()
+        # )
         self.model_optimizer = self.hparams.model_opt_class(
             self.hparams.model.parameters()
         )
         self.optimizers_dict = {
             "model_optimizer": self.model_optimizer,
-            "weights_optimizer": self.weights_optimizer,
+            # "weights_optimizer": self.weights_optimizer,
         }
         # Initializing the weights
         if self.checkpointer is not None:
             self.checkpointer.add_recoverable("modelopt", self.model_optimizer)
-            self.checkpointer.add_recoverable(
-                "weights_opt", self.weights_optimizer
-            )
+            # self.checkpointer.add_recoverable(
+            #     "weights_opt", self.weights_optimizer
+            # )
 
 def dataio_prep(hparams):
     "Creates the datasets and their data processing pipelines."
@@ -255,7 +255,9 @@ if __name__ == "__main__":
         hyperparams_to_save=hparams_file,
         overrides=overrides,
     )
-
+    
+    if hparams['discrete_embedding_layer'].init:
+        hparams['discrete_embedding_layer'].init_embedding(hparams['codec'].vocabulary[:hparams['num_codebooks'],:,:].flatten(0,1))   
     # Dataset prep (parsing GSC and annotation into csv files)
     from prepare_GSC import prepare_GSC
 

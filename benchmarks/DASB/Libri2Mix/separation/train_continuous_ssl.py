Add TER, fix DNSMOS, update gradient accumulation factor

benchmarks/DASB/Libri2Mix/separation/train_continuous_ssl.py
@@ -20,13 +20,13 @@ from hyperpyyaml import load_hyperpyyaml
 from speechbrain.dataio.dataio import write_audio
 from speechbrain.utils.distributed import if_main_process, run_on_main
 
-from train_encodec import Separation as SeparationEncodec, _CACHE
-
 
 warnings.filterwarnings(
     "ignore",
     message="Support for mismatched key_padding_mask and attn_mask is deprecated. Use same type for both instead.",
 )
+
+_CACHE = {}
 
 
 # To use in configuration files
@@ -34,7 +34,7 @@ def len_(num_speakers, SSL_layers, embedding_dim):
     return num_speakers * len(SSL_layers) * embedding_dim
 
 
-class Separation(SeparationEncodec):
+class Separation(sb.Brain):
     @torch.no_grad()
     def sig_to_embs(self, sig, lens):
         # sig: [B, T]
@@ -201,33 +201,36 @@ class Separation(SeparationEncodec):
         hyp_sig = self.embs_to_sig(
             hyp_embs.flatten(end_dim=1)  # [BS, N, K, H]
         )  # [BS, T]
-        if self.hparams.save_audios:
-            rec_sig = self.embs_to_sig(
-                out_embs.flatten(end_dim=1)  # [BS, N, K, H]
-            )  # [BS, T]
+        rec_sig = self.embs_to_sig(
+            out_embs.flatten(end_dim=1)  # [BS, N, K, H]
+        )  # [BS, T]
 
         # Adjust length
         if out_sig.shape[-1] > hyp_sig.shape[-1]:
-            out_sig = out_sig.narrow(-1, 0, hyp_sig.shape[-1])  # [BS, T_min]
+            pad = [0, out_sig.shape[-1] - hyp_sig.shape[-1]]
+            hyp_sig = torch.nn.functional.pad(
+                hyp_sig, pad, mode="replicate"
+            )  # [BS, T_out]
+            rec_sig = torch.nn.functional.pad(
+                rec_sig, pad, mode="replicate"
+            )  # [BS, T_out]
         elif out_sig.shape[-1] < hyp_sig.shape[-1]:
-            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_min]
-            if self.hparams.save_audios:
-                rec_sig = rec_sig.narrow(
-                    -1, 0, out_sig.shape[-1]
-                )  # [BS, T_min]
+            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_out]
+            rec_sig = rec_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_out]
 
         all_IDs = [
             f"{x}_{i}" for x in IDs for i in range(self.hparams.num_speakers)
         ]
         all_lens = lens.repeat_interleave(self.hparams.num_speakers)
         self.dnsmos_metric.append(all_IDs, hyp_sig, all_lens)
+        self.rec_dnsmos_metric.append(all_IDs, rec_sig, all_lens)
+        self.ref_dnsmos_metric.append(all_IDs, out_sig, all_lens)
         self.dwer_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
         self.wavlm_sim_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
         self.ecapatdnn_sim_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
 
         hyp_sig = hyp_sig.reshape(len(hyp_embs), -1)  # [B, ST_min]
-        if self.hparams.save_audios:
-            rec_sig = rec_sig.reshape(len(hyp_embs), -1)  # [B, ST_min]
+        rec_sig = rec_sig.reshape(len(hyp_embs), -1)  # [B, ST_min]
         out_sig = out_sig.reshape(len(hyp_embs), -1)  # [B, ST_min]
 
         if self.hparams.save_audios:
@@ -254,6 +257,67 @@ class Separation(SeparationEncodec):
                     in_sig[i].cpu(),
                     self.hparams.sample_rate,
                 )
+
+    def on_stage_start(self, stage, epoch=None):
+        """Gets called at the beginning of each epoch."""
+        super().on_stage_start(stage, epoch)
+        if stage == sb.Stage.TEST and self.hparams.compute_metrics:
+            self.dnsmos_metric = self.hparams.dnsmos_computer()
+            self.rec_dnsmos_metric = self.hparams.dnsmos_computer()
+            self.ref_dnsmos_metric = self.hparams.dnsmos_computer()
+            self.dwer_metric = self.hparams.dwer_computer()
+            self.wavlm_sim_metric = self.hparams.wavlm_sim_computer()
+            self.ecapatdnn_sim_metric = self.hparams.ecapatdnn_sim_computer()
+
+    def on_stage_end(self, stage, stage_loss, epoch=None):
+        """Gets called at the end of each epoch."""
+        # Compute/store important stats
+        stage_stats = {"loss": stage_loss}
+
+        if stage == sb.Stage.TRAIN:
+            self.train_stats = stage_stats
+
+        # Perform end-of-iteration operations, like annealing, logging, etc.
+        if stage == sb.Stage.VALID:
+            _, lr = self.hparams.scheduler(stage_stats["loss"])
+            sb.nnet.schedulers.update_learning_rate(self.optimizer, lr)
+            steps = self.optimizer_step
+            self.hparams.train_logger.log_stats(
+                stats_meta={"epoch": epoch, "lr": lr, "steps": steps},
+                train_stats=self.train_stats,
+                valid_stats=stage_stats,
+            )
+            self.checkpointer.save_and_keep_only(
+                meta={"loss": stage_stats["loss"]},
+                min_keys=["loss"],
+                num_to_keep=self.hparams.keep_checkpoints,
+            )
+
+        elif stage == sb.Stage.TEST:
+            if self.hparams.compute_metrics:
+                stage_stats["DNSMOS"] = self.dnsmos_metric.summarize("average")
+                stage_stats["RecDNSMOS"] = self.rec_dnsmos_metric.summarize(
+                    "average"
+                )
+                stage_stats["RefDNSMOS"] = self.ref_dnsmos_metric.summarize(
+                    "average"
+                )
+                stage_stats["dWER"] = self.dwer_metric.summarize("error_rate")
+                stage_stats["WavLMSim"] = self.wavlm_sim_metric.summarize(
+                    "average"
+                )
+                stage_stats["ECAPATDNNSim"] = (
+                    self.ecapatdnn_sim_metric.summarize("average")
+                )
+            self.hparams.train_logger.log_stats(
+                stats_meta={"Epoch loaded": self.hparams.epoch_counter.current},
+                test_stats=stage_stats,
+            )
+            if if_main_process():
+                # Save dWER
+                if self.hparams.compute_metrics:
+                    with open(self.hparams.dwer_file, "w") as w:
+                        self.dwer_metric.write_stats(w)
 
 
 if __name__ == "__main__":

benchmarks/DASB/Libri2Mix/separation/train_encodec.py
@@ -170,6 +170,15 @@ class Separation(sb.Brain):
             log_probs = log_probs.movedim(-1, 2)  # [B, N, S, K, C]
             out_toks = out_toks.movedim(-1, 2)  # [B, N, S, K]
 
+        # Compute TER
+        if stage != sb.Stage.TRAIN:
+            self.ter_metric.append(
+                IDs,
+                log_probs.flatten(start_dim=1, end_dim=3),
+                out_toks.flatten(start_dim=1),
+                out_lens,
+            )
+
         # Vocode
         if stage == sb.Stage.TEST and self.hparams.compute_metrics:
             hyp_toks = log_probs.argmax(dim=-1)  # [B, N, S, K]
@@ -185,33 +194,36 @@ class Separation(sb.Brain):
         hyp_sig = self.toks_to_sig(
             hyp_toks.flatten(end_dim=1)  # [BS, N, K]
         )  # [BS, T]
-        if self.hparams.save_audios:
-            rec_sig = self.toks_to_sig(
-                out_toks.flatten(end_dim=1)  # [BS, N, K]
-            )  # [BS, T]
+        rec_sig = self.toks_to_sig(
+            out_toks.flatten(end_dim=1)  # [BS, N, K]
+        )  # [BS, T]
 
         # Adjust length
         if out_sig.shape[-1] > hyp_sig.shape[-1]:
-            out_sig = out_sig.narrow(-1, 0, hyp_sig.shape[-1])  # [BS, T_min]
+            pad = [0, out_sig.shape[-1] - hyp_sig.shape[-1]]
+            hyp_sig = torch.nn.functional.pad(
+                hyp_sig, pad, mode="replicate"
+            )  # [BS, T_out]
+            rec_sig = torch.nn.functional.pad(
+                rec_sig, pad, mode="replicate"
+            )  # [BS, T_out]
         elif out_sig.shape[-1] < hyp_sig.shape[-1]:
-            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_min]
-            if self.hparams.save_audios:
-                rec_sig = rec_sig.narrow(
-                    -1, 0, out_sig.shape[-1]
-                )  # [BS, T_min]
+            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_out]
+            rec_sig = rec_sig.narrow(-1, 0, out_sig.shape[-1])  # [BS, T_out]
 
         all_IDs = [
             f"{x}_{i}" for x in IDs for i in range(self.hparams.num_speakers)
         ]
         all_lens = lens.repeat_interleave(self.hparams.num_speakers)
         self.dnsmos_metric.append(all_IDs, hyp_sig, all_lens)
+        self.rec_dnsmos_metric.append(all_IDs, rec_sig, all_lens)
+        self.ref_dnsmos_metric.append(all_IDs, out_sig, all_lens)
         self.dwer_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
         self.wavlm_sim_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
         self.ecapatdnn_sim_metric.append(all_IDs, hyp_sig, out_sig, all_lens)
 
         hyp_sig = hyp_sig.reshape(len(hyp_toks), -1)  # [B, ST_min]
-        if self.hparams.save_audios:
-            rec_sig = rec_sig.reshape(len(hyp_toks), -1)  # [B, ST_min]
+        rec_sig = rec_sig.reshape(len(hyp_toks), -1)  # [B, ST_min]
         out_sig = out_sig.reshape(len(hyp_toks), -1)  # [B, ST_min]
 
         if self.hparams.save_audios:
@@ -242,8 +254,12 @@ class Separation(sb.Brain):
     def on_stage_start(self, stage, epoch=None):
         """Gets called at the beginning of each epoch."""
         super().on_stage_start(stage, epoch)
+        if stage != sb.Stage.TRAIN:
+            self.ter_metric = self.hparams.ter_computer()
         if stage == sb.Stage.TEST and self.hparams.compute_metrics:
             self.dnsmos_metric = self.hparams.dnsmos_computer()
+            self.rec_dnsmos_metric = self.hparams.dnsmos_computer()
+            self.ref_dnsmos_metric = self.hparams.dnsmos_computer()
             self.dwer_metric = self.hparams.dwer_computer()
             self.wavlm_sim_metric = self.hparams.wavlm_sim_computer()
             self.ecapatdnn_sim_metric = self.hparams.ecapatdnn_sim_computer()
@@ -255,10 +271,12 @@ class Separation(sb.Brain):
 
         if stage == sb.Stage.TRAIN:
             self.train_stats = stage_stats
+        else:
+            stage_stats["TER"] = self.ter_metric.summarize("average") * 100
 
         # Perform end-of-iteration operations, like annealing, logging, etc.
         if stage == sb.Stage.VALID:
-            _, lr = self.hparams.scheduler(stage_stats["loss"])
+            _, lr = self.hparams.scheduler(stage_stats["TER"])
             sb.nnet.schedulers.update_learning_rate(self.optimizer, lr)
             steps = self.optimizer_step
             self.hparams.train_logger.log_stats(
@@ -267,14 +285,20 @@ class Separation(sb.Brain):
                 valid_stats=stage_stats,
             )
             self.checkpointer.save_and_keep_only(
-                meta={"loss": stage_stats["loss"]},
-                min_keys=["loss"],
+                meta={"TER": stage_stats["TER"]},
+                min_keys=["TER"],
                 num_to_keep=self.hparams.keep_checkpoints,
             )
 
         elif stage == sb.Stage.TEST:
             if self.hparams.compute_metrics:
                 stage_stats["DNSMOS"] = self.dnsmos_metric.summarize("average")
+                stage_stats["RecDNSMOS"] = self.rec_dnsmos_metric.summarize(
+                    "average"
+                )
+                stage_stats["RefDNSMOS"] = self.ref_dnsmos_metric.summarize(
+                    "average"
+                )
                 stage_stats["dWER"] = self.dwer_metric.summarize("error_rate")
                 stage_stats["WavLMSim"] = self.wavlm_sim_metric.summarize(
                     "average"

benchmarks/DASB/VoiceBank/enhancement/train_encodec.py
@@ -118,6 +118,15 @@ class Enhancement(sb.Brain):
             length=out_lens,
         )
 
+        # Compute TER
+        if stage != sb.Stage.TRAIN:
+            self.ter_metric.append(
+                IDs,
+                log_probs.flatten(start_dim=1, end_dim=2),
+                out_toks.flatten(start_dim=1),
+                out_lens,
+            )
+
         # Vocode
         if stage == sb.Stage.TEST and self.hparams.compute_metrics:
             hyp_toks = log_probs.argmax(dim=-1)  # [B, N, K]
@@ -128,18 +137,24 @@ class Enhancement(sb.Brain):
     @torch.no_grad()
     def vocode(self, IDs, in_sig, out_sig, hyp_toks, out_toks, lens):
         hyp_sig = self.toks_to_sig(hyp_toks)  # [B, T]
-        if self.hparams.save_audios:
-            rec_sig = self.toks_to_sig(out_toks)  # [B, T]
+        rec_sig = self.toks_to_sig(out_toks)  # [B, T]
 
         # Adjust length
         if out_sig.shape[-1] > hyp_sig.shape[-1]:
-            out_sig = out_sig.narrow(-1, 0, hyp_sig.shape[-1])  # [B, T_min]
+            pad = [0, out_sig.shape[-1] - hyp_sig.shape[-1]]
+            hyp_sig = torch.nn.functional.pad(
+                hyp_sig, pad, mode="replicate"
+            )  # [B, T_out]
+            rec_sig = torch.nn.functional.pad(
+                rec_sig, pad, mode="replicate"
+            )  # [B, T_out]
         elif out_sig.shape[-1] < hyp_sig.shape[-1]:
-            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [B, T_min]
-            if self.hparams.save_audios:
-                rec_sig = rec_sig.narrow(-1, 0, out_sig.shape[-1])  # [B, T_min]
+            hyp_sig = hyp_sig.narrow(-1, 0, out_sig.shape[-1])  # [B, T_out]
+            rec_sig = rec_sig.narrow(-1, 0, out_sig.shape[-1])  # [B, T_out]
 
         self.dnsmos_metric.append(IDs, hyp_sig, lens)
+        self.rec_dnsmos_metric.append(IDs, rec_sig, lens)
+        self.ref_dnsmos_metric.append(IDs, out_sig, lens)
         self.dwer_metric.append(IDs, hyp_sig, out_sig, lens)
         self.wavlm_sim_metric.append(IDs, hyp_sig, out_sig, lens)
         self.ecapatdnn_sim_metric.append(IDs, hyp_sig, out_sig, lens)
@@ -172,8 +187,12 @@ class Enhancement(sb.Brain):
     def on_stage_start(self, stage, epoch=None):
         """Gets called at the beginning of each epoch."""
         super().on_stage_start(stage, epoch)
+        if stage != sb.Stage.TRAIN:
+            self.ter_metric = self.hparams.ter_computer()
         if stage == sb.Stage.TEST and self.hparams.compute_metrics:
             self.dnsmos_metric = self.hparams.dnsmos_computer()
+            self.rec_dnsmos_metric = self.hparams.dnsmos_computer()
+            self.ref_dnsmos_metric = self.hparams.dnsmos_computer()
             self.dwer_metric = self.hparams.dwer_computer()
             self.wavlm_sim_metric = self.hparams.wavlm_sim_computer()
             self.ecapatdnn_sim_metric = self.hparams.ecapatdnn_sim_computer()
@@ -185,10 +204,12 @@ class Enhancement(sb.Brain):
 
         if stage == sb.Stage.TRAIN:
             self.train_stats = stage_stats
+        else:
+            stage_stats["TER"] = self.ter_metric.summarize("average") * 100
 
         # Perform end-of-iteration operations, like annealing, logging, etc.
         if stage == sb.Stage.VALID:
-            _, lr = self.hparams.scheduler(stage_stats["loss"])
+            _, lr = self.hparams.scheduler(stage_stats["TER"])
             sb.nnet.schedulers.update_learning_rate(self.optimizer, lr)
             steps = self.optimizer_step
             self.hparams.train_logger.log_stats(
@@ -197,14 +218,20 @@ class Enhancement(sb.Brain):
                 valid_stats=stage_stats,
             )
             self.checkpointer.save_and_keep_only(
-                meta={"loss": stage_stats["loss"]},
-                min_keys=["loss"],
+                meta={"TER": stage_stats["TER"]},
+                min_keys=["TER"],
                 num_to_keep=self.hparams.keep_checkpoints,
             )
 
         elif stage == sb.Stage.TEST:
             if self.hparams.compute_metrics:
                 stage_stats["DNSMOS"] = self.dnsmos_metric.summarize("average")
+                stage_stats["RecDNSMOS"] = self.rec_dnsmos_metric.summarize(
+                    "average"
+                )
+                stage_stats["RefDNSMOS"] = self.ref_dnsmos_metric.summarize(
+                    "average"
+                )
                 stage_stats["dWER"] = self.dwer_metric.summarize("error_rate")
                 stage_stats["WavLMSim"] = self.wavlm_sim_metric.summarize(
                     "average"
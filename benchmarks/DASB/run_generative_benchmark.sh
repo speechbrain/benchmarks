29e3babf0efecbe5decced3894d68ba5bd31c55f
flexthink <flexthink@users.noreply.github.com>
2024-07-23 13:59:07 -0400

    DASB: Tokotron: Update generative script

--- benchmarks/DASB/run_generative_benchmark.sh
+++ benchmarks/DASB/run_generative_benchmark.sh
@@ -2,13 +2,57 @@
 # Please consult the README.md file for instructions on how to run the benchmark.
 
 tokenizer_name=$1
-output_folder='/path/to/output'
-declare -a DatasetsFolders=('path/to/Libri2Mix' 'path/to/VoiceBank')
-declare -a ConsideredTasks=('Libri2Mix/separation' 'VoiceBank/enhancement')
-declare -a DownStreams=('conformer' 'conformer')
+output_folder='path/to/output'
+librimix_path='path/to/Libri2Mix'
+voicebank_path='path/to/VoiceBank'
+ljspeech_path='path/to/ljspeech'
+
+declare -a DatasetsFolders=(\
+        "$librimix_path" \
+        "$voicebank_path" \
+        "$ljspeech_path" \
+        "$ljspeech_path" \
+)
+declare -a ConsideredTasks=(\
+        'Libri2Mix/separation' \
+        'VoiceBank/enhancement' \
+        'LJSpeech/TTS' \
+        'LJSpeech/TTS' \
+)
+declare -a DownStreams=(\
+        'conformer' \
+        'conformer' \
+        'tokotron' \
+        'tokotron' \
+)
+declare -a ExtraArgs=(\
+        '' \
+        '' \
+        '' \
+        '--enc_num_layers 3 --dec_num_layers 6' \
+)
+
+declare -a OutputSuffix=(\
+        '' \
+        '' \
+        '' \
+        '-small'
+)
+
+script_dir=$(pwd)
+
 for i in "${!ConsideredTasks[@]}"; do
         task=${ConsideredTasks[i]}
         downstream=${DownStreams[i]}
         dataset_folder=${DatasetsFolders[i]}
-        python $task/$downstream/train_$tokenizer_name.py $task/$downstream/hparams/train_$tokenizer_name.yaml   --output_folder $output_folder/$tokenizer_name/$task/$downstream --data_folder $dataset_folder
+        extra_args=${ExtraArgs[i]}
+        suffix=${OutputSuffix[i]}
+        set -- $extra_args
+        cd $task/$downstream
+        python train_$tokenizer_name.py \
+                hparams/train_$tokenizer_name.yaml  \
+                --output_folder $output_folder/$tokenizer_name/$task/$downstream$suffix \
+                --data_folder $dataset_folder \
+                $@
+        cd $script_dir
 done

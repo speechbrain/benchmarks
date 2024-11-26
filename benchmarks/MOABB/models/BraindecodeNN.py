669d19ed93d4ab28ba3ad0b0098bf7e82b808b2e
Victor C <55927568+vmcru@users.noreply.github.com>
2024-11-26 11:13:04 -0500

    Update BraindecodeNN.py with header descriptions & auths
    The following items were added to the header:
    Braindecode description
    Notes
    Authors

--- benchmarks/MOABB/models/BraindecodeNN.py
+++ benchmarks/MOABB/models/BraindecodeNN.py
@@ -1,3 +1,23 @@
+"""Braindecode from https://braindecode.org/stable/index.html.
+Braindecode is an open-source Python toolbox for decoding raw electrophysiological brain data with 
+deep learning models. It includes dataset fetchers, data preprocessing and visualization tools, as 
+well as implementations of several deep learning architectures and data augmentations for analysis 
+of EEG, ECoG and MEG.
+
+This code is a Speechbrain interface for the Braindecode models. This wrapper allows the usage of 
+Braindecode models with the benchmarks pipeline for experiment reproducibility. 
+
+Note 1: The library "einops" is included when braindecode is installed and is not in the package 
+requirements of benchmarks or speechbrain. 
+
+Note 2: Softmax is added to the model layer stack since NLL is used.
+
+Authors
+ * Davide Borra, 2023
+ * Drew Wagner, 2024
+ * Victor Cruz, 2024
+ * Bruno Aristimunha, 2024
+"""
 import torch
 from einops.layers.torch import Rearrange
 

speechbrain/benchmarks

commit 45b12f1764e31e6f408da53753023ebe9b2117fe

Update CL-MASR to new SpeechBrain version (#14)
* Update to new SpeechBrain version

* Update README.md

* update installation instruction

---------

Co-authored-by: Mirco Ravanelli <mirco.ravanelli@gmail.com>

benchmarks/CL_MASR/wavlm/train_agem.py
@@ -120,9 +120,9 @@ class ASR(sb.Brain):
     def fit_batch(self, batch):
         """Fit one batch."""
         # Managing automatic mixed precision
-        if self.auto_mix_prec:
+        if self.use_amp:
             # Compute gradient
-            with torch.cuda.amp.autocast(self.auto_mix_prec):
+            with torch.cuda.amp.autocast(self.use_amp):
                 outputs = self.compute_forward(batch, sb.Stage.TRAIN)
                 loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
             with self.no_sync(False):
@@ -143,7 +143,7 @@ class ASR(sb.Brain):
                 batch = next(self.replay_data_iter)
 
             # Compute reference gradient
-            with torch.cuda.amp.autocast(self.auto_mix_prec):
+            with torch.cuda.amp.autocast(self.use_amp):
                 outputs = self.compute_forward(batch, sb.Stage.TRAIN)
                 loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
             with self.no_sync(False):
@@ -175,7 +175,7 @@ class ASR(sb.Brain):
                         param.grad = None
 
             self.scaler.unscale_(self.optimizer)
-            if self.check_gradients(loss):
+            if self.check_gradients():
                 self.scaler.step(self.optimizer)
             self.scaler.update()
             self.zero_grad()
@@ -232,7 +232,7 @@ class ASR(sb.Brain):
                     else:
                         param.grad = None
 
-            if self.check_gradients(loss):
+            if self.check_gradients():
                 self.optimizer.step()
             self.zero_grad()
             self.optimizer_step += 1

benchmarks/CL_MASR/wavlm/train_ewc.py
@@ -183,10 +183,10 @@ def compute_ewc_params(hparams, run_opts, locales):
     batch_size = hparams["train_dataloader_kwargs"].get("batch_size", 1)
     max_grad_norm = hparams.get("max_grad_norm", 5.0)
     grad_accumulation_factor = hparams.get("grad_accumulation_factor", 1)
-    auto_mix_prec = hparams.get("auto_mix_prec", False)
+    precision = hparams.get("precision", "fp32")
     hparams["train_dataloader_kwargs"]["batch_size"] = 1
     hparams["max_grad_norm"] = float("inf")
-    hparams["auto_mix_prec"] = False
+    hparams["precision"] = "fp32"
     hparams["grad_accumulation_factor"] = 1
 
     # Multi-gpu (ddp) save data preparation
@@ -221,7 +221,7 @@ def compute_ewc_params(hparams, run_opts, locales):
 
     hparams["train_dataloader_kwargs"]["batch_size"] = batch_size
     hparams["max_grad_norm"] = max_grad_norm
-    hparams["auto_mix_prec"] = auto_mix_prec
+    hparams["precision"] = precision
     hparams["grad_accumulation_factor"] = grad_accumulation_factor
 
     return params, fisher

benchmarks/CL_MASR/wavlm/train_mas.py
@@ -187,10 +187,10 @@ def compute_mas_params(hparams, run_opts, locales):
     batch_size = hparams["train_dataloader_kwargs"].get("batch_size", 1)
     max_grad_norm = hparams.get("max_grad_norm", 5.0)
     grad_accumulation_factor = hparams.get("grad_accumulation_factor", 1)
-    auto_mix_prec = hparams.get("auto_mix_prec", False)
+    precision = hparams.get("precision", "fp32")
     hparams["train_dataloader_kwargs"]["batch_size"] = 1
     hparams["max_grad_norm"] = float("inf")
-    hparams["auto_mix_prec"] = False
+    hparams["precision"] = "fp32"
     hparams["grad_accumulation_factor"] = 1
 
     # Multi-gpu (ddp) save data preparation
@@ -225,7 +225,7 @@ def compute_mas_params(hparams, run_opts, locales):
 
     hparams["train_dataloader_kwargs"]["batch_size"] = batch_size
     hparams["max_grad_norm"] = max_grad_norm
-    hparams["auto_mix_prec"] = auto_mix_prec
+    hparams["precision"] = precision
     hparams["grad_accumulation_factor"] = grad_accumulation_factor
 
     return params, importance

benchmarks/CL_MASR/whisper/train_agem.py
@@ -131,9 +131,9 @@ class ASR(sb.Brain):
     def fit_batch(self, batch):
         """Fit one batch."""
         # Managing automatic mixed precision
-        if self.auto_mix_prec:
+        if self.use_amp:
             # Compute gradient
-            with torch.cuda.amp.autocast(self.auto_mix_prec):
+            with torch.cuda.amp.autocast(self.use_amp):
                 outputs = self.compute_forward(batch, sb.Stage.TRAIN)
                 loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
             with self.no_sync(False):
@@ -156,7 +156,7 @@ class ASR(sb.Brain):
                 batch = next(self.replay_data_iter)
 
             # Compute reference gradient
-            with torch.cuda.amp.autocast(self.auto_mix_prec):
+            with torch.cuda.amp.autocast(self.use_amp):
                 outputs = self.compute_forward(batch, sb.Stage.TRAIN)
                 loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
             with self.no_sync(False):
@@ -183,7 +183,7 @@ class ASR(sb.Brain):
                         start = end
 
             self.scaler.unscale_(self.optimizer)
-            if self.check_gradients(loss):
+            if self.check_gradients():
                 self.scaler.step(self.optimizer)
             self.scaler.update()
             self.zero_grad()
@@ -237,7 +237,7 @@ class ASR(sb.Brain):
                         param.grad = grad[start:end].reshape_as(param)
                         start = end
 
-            if self.check_gradients(loss):
+            if self.check_gradients():
                 self.optimizer.step()
             self.zero_grad()
             self.optimizer_step += 1

benchmarks/CL_MASR/whisper/train_ewc.py
@@ -203,10 +203,10 @@ def compute_ewc_params(hparams, run_opts, locales):
     batch_size = hparams["train_dataloader_kwargs"].get("batch_size", 1)
     max_grad_norm = hparams.get("max_grad_norm", 5.0)
     grad_accumulation_factor = hparams.get("grad_accumulation_factor", 1)
-    auto_mix_prec = hparams.get("auto_mix_prec", False)
+    precision = hparams.get("precision", "fp32")
     hparams["train_dataloader_kwargs"]["batch_size"] = 1
     hparams["max_grad_norm"] = float("inf")
-    hparams["auto_mix_prec"] = False
+    hparams["precision"] = "fp32"
     hparams["grad_accumulation_factor"] = 1
 
     # Multi-gpu (ddp) save data preparation
@@ -242,7 +242,7 @@ def compute_ewc_params(hparams, run_opts, locales):
 
     hparams["train_dataloader_kwargs"]["batch_size"] = batch_size
     hparams["max_grad_norm"] = max_grad_norm
-    hparams["auto_mix_prec"] = auto_mix_prec
+    hparams["precision"] = precision
     hparams["grad_accumulation_factor"] = grad_accumulation_factor
 
     return params, fisher

benchmarks/CL_MASR/whisper/train_mas.py
@@ -205,10 +205,10 @@ def compute_mas_params(hparams, run_opts, locales):
     batch_size = hparams["train_dataloader_kwargs"].get("batch_size", 1)
     max_grad_norm = hparams.get("max_grad_norm", 5.0)
     grad_accumulation_factor = hparams.get("grad_accumulation_factor", 1)
-    auto_mix_prec = hparams.get("auto_mix_prec", False)
+    precision = hparams.get("precision", "fp32")
     hparams["train_dataloader_kwargs"]["batch_size"] = 1
     hparams["max_grad_norm"] = float("inf")
-    hparams["auto_mix_prec"] = False
+    hparams["precision"] = "fp32"
     hparams["grad_accumulation_factor"] = 1
 
     # Multi-gpu (ddp) save data preparation
@@ -244,7 +244,7 @@ def compute_mas_params(hparams, run_opts, locales):
 
     hparams["train_dataloader_kwargs"]["batch_size"] = batch_size
     hparams["max_grad_norm"] = max_grad_norm
-    hparams["auto_mix_prec"] = auto_mix_prec
+    hparams["precision"] = precision
     hparams["grad_accumulation_factor"] = grad_accumulation_factor
 
     return params, importance

benchmarks/CL_MASR/wavlm/hparams/pretrain.yaml
@@ -49,7 +49,7 @@ bidirectional: True
 max_target_length: 448  # Must be <= 448 (Whisper maximum target length)
 nonfinite_patience: 10
 max_grad_norm: 5.0
-auto_mix_prec: True
+precision: fp16
 gradient_checkpointing: False
 ckpt_interval_minutes: 600
 

benchmarks/CL_MASR/wavlm/hparams/train_agem.yaml
@@ -52,7 +52,7 @@ bidirectional: True
 max_target_length: 448  # Must be <= 448 (Whisper maximum target length)
 nonfinite_patience: 10
 max_grad_norm: 5.0
-auto_mix_prec: True
+precision: fp16
 gradient_checkpointing: False
 ckpt_interval_minutes: 600
 

commit 8c752ac753421f52e9dcc7b1ae82a12a5cc77fc9

Update CL-MASR (#49)
* Update readme

* Fix bugs introduced in 45b12f1764e31e6f408da53753023ebe9b2117fe

* Fix formatting

* Update readme

benchmarks/CL_MASR/wavlm/train_agem.py
@@ -175,8 +175,8 @@ class ASR(sb.Brain):
                         param.grad = None
 
             self.scaler.unscale_(self.optimizer)
-            if self.check_gradients():
-                self.scaler.step(self.optimizer)
+            self.check_gradients()
+            self.scaler.step(self.optimizer)
             self.scaler.update()
             self.zero_grad()
             self.optimizer_step += 1
@@ -232,8 +232,8 @@ class ASR(sb.Brain):
                     else:
                         param.grad = None
 
-            if self.check_gradients():
-                self.optimizer.step()
+            self.check_gradients()
+            self.optimizer.step()
             self.zero_grad()
             self.optimizer_step += 1
 

benchmarks/CL_MASR/wavlm/train_ewc.py
@@ -63,7 +63,7 @@ class ASR(sb.Brain):
 
         if stage == sb.Stage.TRAIN and hasattr(self.hparams, "ewc_params"):
             for name, param in self.modules.wavlm.named_parameters():
-                if not param.requires_grad or param.grad is None:
+                if not param.requires_grad:
                     continue
                 old_param, fisher = self.hparams.ewc_params
                 loss += (

benchmarks/CL_MASR/wavlm/train_mas.py
@@ -63,7 +63,7 @@ class ASR(sb.Brain):
 
         if stage == sb.Stage.TRAIN and hasattr(self.hparams, "mas_params"):
             for name, param in self.modules.wavlm.named_parameters():
-                if not param.requires_grad or param.grad is None:
+                if not param.requires_grad:
                     continue
                 old_param, importance = self.hparams.mas_params
                 loss += (

benchmarks/CL_MASR/whisper/train_agem.py
@@ -183,8 +183,8 @@ class ASR(sb.Brain):
                         start = end
 
             self.scaler.unscale_(self.optimizer)
-            if self.check_gradients():
-                self.scaler.step(self.optimizer)
+            self.check_gradients()
+            self.scaler.step(self.optimizer)
             self.scaler.update()
             self.zero_grad()
             self.optimizer_step += 1
@@ -237,8 +237,8 @@ class ASR(sb.Brain):
                         param.grad = grad[start:end].reshape_as(param)
                         start = end
 
-            if self.check_gradients():
-                self.optimizer.step()
+            self.check_gradients()
+            self.optimizer.step()
             self.zero_grad()
             self.optimizer_step += 1
 

benchmarks/CL_MASR/whisper/train_ewc.py
@@ -66,7 +66,7 @@ class ASR(sb.Brain):
 
         if stage == sb.Stage.TRAIN and hasattr(self.hparams, "ewc_params"):
             for name, param in self.modules.whisper.named_parameters():
-                if not param.requires_grad or param.grad is None:
+                if not param.requires_grad:
                     continue
                 old_param, fisher = self.hparams.ewc_params
                 if "embed_tokens.weight" in name:

benchmarks/CL_MASR/whisper/train_mas.py
@@ -65,7 +65,7 @@ class ASR(sb.Brain):
 
         if stage == sb.Stage.TRAIN and hasattr(self.hparams, "mas_params"):
             for name, param in self.modules.whisper.named_parameters():
-                if not param.requires_grad or param.grad is None:
+                if not param.requires_grad:
                     continue
                 old_param, importance = self.hparams.mas_params
                 if "embed_tokens.weight" in name:
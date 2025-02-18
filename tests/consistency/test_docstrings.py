c0e92d8b5acdf98a476e7a177ab803d547fe5deb
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-02-18 15:02:24 +0100

    [FIX] fixing the test function

--- tests/consistency/test_docstrings.py
+++ tests/consistency/test_docstrings.py
@@ -2,10 +2,11 @@
 
 Authors
  * Mirco Ravanelli 2022
+ * Bruno Aristimunha 2025
 """
 from tests.utils.check_docstrings import check_docstrings
 
 
-def test_recipe_list(base_folder="."):
-    check_folders = ["speechbrain", "tools", "templates"]
+def test_recipe_list(base_folder="../.."):
+    check_folders = ["speechbrain", "tools", "benchmarks/MOABB"]
     assert check_docstrings(base_folder, check_folders)

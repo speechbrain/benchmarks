6de5acbd32962bd97b9e069128cd9c077b2cae38
flexthink <flexthink@users.noreply.github.com>
2025-05-13 20:23:11 -0400

    DASB: WER/CER fix

--- benchmarks/DASB/utils/eval.py
+++ benchmarks/DASB/utils/eval.py
@@ -483,8 +483,10 @@ class WhisperASRSpeechEvaluator(ASRSpeechEvaluator):
         predicted_words = [self.normalize(text) for text in predicted_words]
         ids = range(1, len(wavs) + 1)
         wer_metric, cer_metric = init_asr_metrics()
-        wer_metric.append(ids, predicted_words, text)
-        cer_metric.append(ids, predicted_words, text)
+        predicted_words_split = [item.split(" ") for item in predicted_words]
+        text_split = [item.split(" ") for item in text]
+        wer_metric.append(ids, predicted_words_split, text_split)
+        cer_metric.append(ids, predicted_words_split, text_split)
         wer = torch.tensor(
             [score["WER"] for score in wer_metric.scores], device=wavs.device
         )
